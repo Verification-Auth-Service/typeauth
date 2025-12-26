e15a92330bd7230fd82b8e32e0045e9c3085bb77
maruyama <taku2000maru@icloud.com>
2025-12-26 22:08:42 +0900

    createCheckerの分離

--- apps/static-analyzer/src/test/event.spec.ts
+++ apps/static-analyzer/src/test/event.spec.ts
@@ -3,30 +3,9 @@ import ts from "typescript";
 import { extractEvents } from "../analyze/event";
 import { PEvent } from "../types/event";
 import { describe, it, expect } from "vitest";
+import { createSourceFile, createChecker } from "./helper/create";
 
 describe("extractEventsのテスト", () => {
-  const createSourceFile = (code: string) => ts.createSourceFile("test.ts", code, ts.ScriptTarget.Latest, true, ts.ScriptKind.TS);
-
-  const createChecker = (sourceFile: ts.SourceFile) => {
-    const program = ts.createProgram({
-      rootNames: [sourceFile.fileName],
-      options: {},
-      host: {
-        fileExists: (fileName) => fileName === sourceFile.fileName,
-        readFile: (fileName) => (fileName === sourceFile.fileName ? sourceFile.getFullText() : undefined),
-        getSourceFile: (fileName, _) => (fileName === sourceFile.fileName ? sourceFile : undefined),
-        getDefaultLibFileName: () => "lib.d.ts",
-        writeFile: () => {},
-        getCurrentDirectory: () => "",
-        getDirectories: () => [],
-        getCanonicalFileName: (fileName) => fileName,
-        useCaseSensitiveFileNames: () => true,
-        getNewLine: () => "\n",
-      },
-    });
-    return program.getTypeChecker();
-  };
-
   it("if文のイベント抽出", () => {
     const code = `
       function test(x: number) {
